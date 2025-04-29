4751c96c6406e0a8af66cb83edd15748cbc2c2d9
ram <rammalathi7301@gmail.com>
2025-04-29 21:37:17 +0530

    imagefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['https://main.d109a5j74rx26i.amplifyapp.com/', 'main.d109a5j74rx26i.amplifyapp.com'],
-      },
+  images: {
+    domains: ['main.d109a5j74rx26i.amplifyapp.com'],
+  },
 };
 
 export default nextConfig;
