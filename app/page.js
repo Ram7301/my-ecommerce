858987eb54bc527fbcb7035258639df1990253c4
ram <rammalathi7301@gmail.com>
2025-04-29 21:28:22 +0530

    imagefix

--- app/page.js
+++ app/page.js
@@ -6,11 +6,11 @@ import { Carousel } from "@/components/carousel";
 
 export default async function Home() {
   const products = [
-    { id: 2, type:"laptops", category: "Electronics", name: "MacBook Air M3", price: 1299.99, image: "/images/electronics/macbookairm3.jpg" },
+    { id: 2, type:"laptops", category: "Electronics", name: "MacBook Air M3", price: 1299.99, image: "/images/macbookairm3.jpg" },
     
-    { id: 11, type:"men", category: "Clothing", name: "Men's Leather Jacket", price: 199.99, image: "/images/clothings/mens-leather-jacket.jpg" },
-    { id: 12, type:"women", category: "Clothing", name: "Women's Summer Dress", price: 79.99, image: "/images/clothings/womens-summer-dress.jpg" },
-    { id: 13, type:"kids", category: "Clothing", name: "Kids Hoodie", price: 39.99, image: "/images/clothings/kids-hoodie.jpg" },
+    { id: 11, type:"men", category: "Clothing", name: "Men's Leather Jacket", price: 199.99, image: "/images/mens-leather-jacket.jpg" },
+    { id: 12, type:"women", category: "Clothing", name: "Women's Summer Dress", price: 79.99, image: "/images/womens-summer-dress.jpg" },
+    { id: 13, type:"kids", category: "Clothing", name: "Kids Hoodie", price: 39.99, image: "/images/kids-hoodie.jpg" },
 
   
   ];
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['https://main.d109a5j74rx26i.amplifyapp.com'],
+        domains: ['https://main.d109a5j74rx26i.amplifyapp.com/', 'main.d109a5j74rx26i.amplifyapp.com'],
       },
 };
 
