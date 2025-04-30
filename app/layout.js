05706fc3a502dc1efa8c5d78ef49d3bc0fb3eabd
ram <rammalahi7301@gmail.com>
2025-04-30 09:36:09 +0530

    final

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 export const metadata = {
-  title: 'Ecommerce App',
+  title: 'ShopZone',
 };
 
 export default function RootLayout({ children }) {
